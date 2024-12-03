3f7e0b39c68998f47b23d4691175b5b4fc0b8069
Kai Weston <230148563@aston.ac.uk>
2024-12-03 14:19:29 +0000

    Changed twitter.png to twitter-logo.png

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -81,7 +81,7 @@
       <div class="socials">
         <p>Follow Us</p>
         <a href="#"><img src="{{asset('images/facebook.png')}}" alt="Facebook"></a>
-        <a href="#"><img src="{{asset('images/twitter.png')}}" alt="Twitter"></a>
+        <a href="#"><img src="{{asset('images/twitter-logo.png')}}" alt="Twitter"></a>
         <a href="#"><img src="{{asset('images/instagram.png')}}" alt="Instagram"></a>
       </div>
       <div class="newsletter">
