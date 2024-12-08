f7d3e72601a4e2b86c4d1fbf0ceaee08fb747fab
Jack Beckley <230121625@aston.ac.uk>
2024-12-08 00:14:09 +0000

    updated routes in web.php to include new naviagtion links

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,42 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Default route (welcome page)
 Route::get('/', function () {
     return view('welcome');
 });
+
+// About Us route
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+// Contact Us route
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+// Shop route
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
+
+// Profile route (Account main page)
+Route::get('/profile', function () {
+    return view('profile');
+})->name('profile');
+
+// Login route
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+// Admin Dashboard route
+Route::get('/admin_dashboard', function () {
+    return view('admin_dashboard');
+})->name('admin_dashboard');
+
+// Cart route
+Route::get('/cart', function () {
+    return view('cart');
+})->name('cart');
