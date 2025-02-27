c8e717cd655817cec4bb1ee380474468ad394fa0
ibraheem navid <230231980@aston.ac.uk>
2025-02-27 15:46:22 +0000

    made changes to contact us css to help fix footer layout

--- resources/views/Contact.blade.php
+++ resources/views/Contact.blade.php
@@ -23,18 +23,22 @@
                     <label for="name">Name:</label>
                     <input type="text" id="name" name="name" required placeholder="Enter your name">
                 </div>
+                <br>
                 <div class="form-group">
                     <label for="email">Email Address:</label>
                     <input type="email" id="email" name="email" required placeholder="Enter your email">
                 </div>
+                <br>
                 <div class="form-group">
                     <label for="phone">Phone:</label>
                     <input type="tel" id="phone" value="" required="" name="phone" required placeholder="Enter your phone number" maxlength="11" minlength="10">
                 </div>
+                <br>
                 <div class="form-group">
                     <label for="message">Message:</label>
                     <textarea id="message" name="message" required placeholder="Write your message here"></textarea>
                 </div>
+                <br>
                 <button type="submit" class="button">Submit</button>
             </form>
         </div>
