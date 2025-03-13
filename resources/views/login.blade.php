8227dcc0db4b90499fb874a484724fe02a0d3574
Rajveer Dhami <230176290@aston.ac.uk>
2025-03-13 15:23:16 +0000

    changed file names, changed IDs and names to allow inputs to be recieved in database

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,25 +8,7 @@
         <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     </head>
     <body>
-        <header>
-            <!-- /*jacks nav bar*/ -->
-            <nav class="navbar">
-                <div class="logo">
-                  <a href="about_us.html">
-                     <!--<img src="Astonic logo.webp" alt="Astonic Sports Logo"> -->
-                   <img src="{{ asset('images/Astonic logo.webp') }}" alt="Astonic Sports Logo">
-                  </a>
-                </div>
-                <ul class="nav-links">
-                  <li><a href="Home.html">Home</a></li>
-                  <li><a href="about_us.html">About Us</a></li>
-                  <li><a href="contact_us.html">Contact Us</a></li>
-                  <li><a href="product_listing.html">Shop</a></li>
-                  <li><a href="login.html">Account</a></li>
-                  <li><a href="cart.html">Cart</a></li>
-                </ul>
-              </nav>
-        </header>
+ 
         <div class="big-container">
         <div class="login-container">
         <h1>Login</h1>
@@ -62,45 +44,7 @@
 </form>
 </div>
 </div> 
-  <footer class="footer">
- <!-- Footer made by ibraheem -->
- <div class="footer-container">
-
-  <!--Social Links-->
-  <div class="socials">
-      <p>FOLLOW US ON SOCIALS</p>
-      <br>
-      <div class="social-links">
-          <a href="#"aria-label="Follow us on Facebook"></a> <!--<img src="facebook-logo.png" alt="Facebook"></a> --> <img src="{{ asset('images/facebook-logo.png') }}" alt="Facebook">
-          <a href="#"aria-label="Follow us on Twitter"></a> <!--<img src="twitter-logo.png" alt="Twitter"></a> --> <img src="{{ asset('images/twitter-logo.png') }}" alt="Twitter">
-          <a href="#"aria-label="Follow us on Instagram"></a><!--<img src="instagram-logo.png" alt="Instagram"></a> --> <img src="{{ asset('images/instagram-logo.png') }}" alt="Instagram">
-      </div>
-  </div>
-
-  <!--Newsletter Signup-->
-  <div class="newsletter">
-      <h4>SIGN UP TO OUR NEWSLETTER</h4>
-      <p>Sign up for the latest updates on product releases &amp; discounts!</p>
-      <form action="email">
-          <input value="" name="EMAIL" class="email" id="footer-EMAIL" placeholder="Enter Email Address" required="" type="email">
-         <br><br>
-          <input value="Join" class="button" type="submit">
-      </form>
-      <p>&copy; 2024 Astonic Sports. All Rights Reserved.</p>
-  </div>
-  <!--Nav Section-->
-<div class="footer-nav">
-  <ul>
-      <li><a href="Home.html">Home</a></li>
-      <li><a href="about_us.html">About Us</a></li>
-      <li><a href="contact_us.html">Contact Us</a></li>
-      <li><a href="product_listing.html">Shop</a></li>
-      <li><a href="login.html">Account</a></li>
-      <li><a href="index.html">Cart</a></li>
-  </ul>
-</div>
-</div>
-</footer> 
-<script src="Login section.js"></script>
+ 
+<script src="login.js"></script>
     </body>
     </html>
--- resources/views/resetpassword.blade.php
+++ resources/views/resetpassword.blade.php
@@ -8,25 +8,7 @@
         <link rel="stylesheet" href="{{ asset('css/resetpassword.css') }}">
     </head>
     <body>
-        <header>
-            <nav class="navbar">
-                <div class="logo">
-                  <a href="about_us.html">
-                   <!--<img src="Astonic logo.webp" alt="Astonic Sports Logo"> -->
-                   <img src="{{ asset('images/Astonic logo.webp') }}" alt="Astonic Sports Logo">
-                  </a>
-                  
-                </div>
-                <ul class="nav-links">
-                  <li><a href="Home.html">Home</a></li>
-                  <li><a href="about_us.html">About Us</a></li>
-                  <li><a href="contact_us.html">Contact Us</a></li>
-                  <li><a href="product_listing.html">Shop</a></li>
-                  <li><a href="login.html">Account</a></li>
-                  <li><a href="cart.html">Cart</a></li>
-                </ul>
-              </nav>
-        </header>
+    
         <main class ="main">
         <h1>Reset Password</h1>
      <p> To reset your password please enter your email address below.</p>
@@ -36,45 +18,7 @@
        <br> <button type="Reset password">Reset password</button>
      </form>
     </main>
-     <footer class="footer">
-     <!-- Footer made by ibraheem -->
-    <div class="footer-container">
-
-      <!--Social Links-->
-      <div class="socials">
-          <p>FOLLOW US ON SOCIALS</p>
-          <br>
-          <div class="social-links">
-            <a href="#"aria-label="Follow us on Facebook"></a> <!--<img src="facebook-logo.png" alt="Facebook"></a> --> <img src="{{ asset('images/facebook-logo.png') }}" alt="Facebook">
-            <a href="#"aria-label="Follow us on Twitter"></a> <!--<img src="twitter-logo.png" alt="Twitter"></a> --> <img src="{{ asset('images/twitter-logo.png') }}" alt="Twitter">
-            <a href="#"aria-label="Follow us on Instagram"></a><!--<img src="instagram-logo.png" alt="Instagram"></a> --> <img src="{{ asset('images/instagram-logo.png') }}" alt="Instagram">
-          </div>
-      </div>
-
-      <!--Newsletter Signup-->
-      <div class="newsletter">
-          <h4>SIGN UP TO OUR NEWSLETTER</h4>
-          <p>Sign up for the latest updates on product releases &amp; discounts!</p>
-          <form action="email">
-              <input value="" name="EMAIL" class="email" id="footer-EMAIL" placeholder="Enter Email Address" required="" type="email">
-             <br><br>
-              <input value="Join" class="button" type="submit">
-          </form>
-          <p>&copy; 2024 Astonic Sports. All Rights Reserved.</p>
-      </div>
-      <!--Nav Section-->
-  <div class="footer-nav">
-      <ul>
-          <li><a href="Home.html">Home</a></li>
-          <li><a href="about_us.html">About Us</a></li>
-          <li><a href="contact_us.html">Contact Us</a></li>
-          <li><a href="product_listing.html">Shop</a></li>
-          <li><a href="login.html">Account</a></li>
-          <li><a href="index.html">Cart</a></li>
-      </ul>
-  </div>
-  </div>
-   </footer> 
+     
     </body>
     <script src="forgotten password.js"></script>
 </html>
