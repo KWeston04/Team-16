2b82378bf35068e8a2b3eab1c098a2377e6d3e77
Talal9952 <230052042@aston.ac.uk>
2025-03-22 01:06:05 +0000

    create an observer to send a notification on each new record creation process and set that observer in the ContactUsForm modal

---
+++ app/Observers/ContactUsFormObserver.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Observers;
+
+use App\Filament\Resources\ContactUsFormResource;
+use App\Models\ContactUsForm;
+use App\Models\User;
+use Filament\Notifications\Actions\Action;
+use Filament\Notifications\Notification;
+
+class ContactUsFormObserver
+{
+        /**
+     * Handle the ContactUsForm "created" event.
+     */
+    public function created(ContactUsForm $contactUsForm): void
+    {
+        // Get all users with type 'Admin'
+        $admins = User::where('user_type', 'admin')->get();
+
+        // Send notification to each admin
+        Notification::make()
+            ->icon('heroicon-o-inbox')
+            ->iconColor('info')
+            ->title('New Contact Request.')
+            ->body('From: ' . $contactUsForm->email . '.')
+            ->actions([
+                Action::make('View')
+                    ->url(ContactUsFormResource::getUrl('edit', ['record' => $contactUsForm])),
+                Action::make('markRead')
+                    ->markAsRead()
+                    ->color('primary'),
+                Action::make('markUnread')
+                    ->markAsUnread()
+                    ->color(color: 'warning'),
+            ])
+            ->sendToDatabase($admins);
+    }
+}
